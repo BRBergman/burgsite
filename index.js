72336aee6834177406fd0add69fb8623af95ed76
BRBergman <blake.bergman@gmail.com>
2024-09-21 20:09:13 -0400

    js not working on localhost so we push to prod

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
+const element = document.getElementById("qotd");
+element.innerHTML = "New Heading";
+
 var SaulGoodmanBwawh = new Audio('/indexassets/SaulGoodmanBwawh.mp3');
 function SaulGoodmanBwawhAudio()
 {
     SaulGoodmanBwawh.volume = 0.2;
     SaulGoodmanBwawh.play();
 }
-
 function select_random_quote()
 {
     let file = [test1,test2,test3];
     let index = Math.round(Math.random()*file.length);
+    document.getElementById("qotd").innerHTML = file[index];
 }
