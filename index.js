406b8f8377e74abaf29bf562702aadd539c2e3cf
BRBergman <blake.bergman@gmail.com>
2024-09-23 15:59:09 -0400

    made sentecne of the refresh work on main

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-var SaulGoodmanBwawh = new Audio('/indexassets/SaulGoodmanBwawh.mp3');
+const SaulGoodmanBwawh = new Audio('/indexassets/SaulGoodmanBwawh.mp3');
 function SaulGoodmanBwawhAudio() {
     SaulGoodmanBwawh.volume = 0.2;
     SaulGoodmanBwawh.play();
 }
+
+const file = ["bwaaa", "DO NOT THROW SOULS", "you're out of touch","haiii"];
+//todo: make read from seperate file that i can change without updating the website
 function select_random_quote() {
-    let file = ["test1", "test2", "test3"];
     let index = Math.floor(Math.random() * file.length);
     let quote = file[index];
     document.getElementById("qotd").innerHTML = quote;
     return quote;
 }
-let x = select_random_quote();
+select_random_quote();
