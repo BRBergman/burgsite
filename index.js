61e6b244e3a6777bb0e24a55a962c569849a13e3
BRBergman <blake.bergman@gmail.com>
2024-09-23 09:24:00 -0400

    i forgot to put quotes in "" lmao

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 var SaulGoodmanBwawh = new Audio('/indexassets/SaulGoodmanBwawh.mp3');
-function SaulGoodmanBwawhAudio()
-{
+function SaulGoodmanBwawhAudio() {
     SaulGoodmanBwawh.volume = 0.2;
     SaulGoodmanBwawh.play();
 }
-function select_random_quote()
-{
-    let file = [test1,test2,test3];
-    let index = Math.round(Math.random()*file.length);
-    document.getElementById("qotd").innerHTML = file[index];
+function select_random_quote() {
+    let file = ["test1", "test2", "test3"];
+    let index = Math.floor(Math.random() * file.length);
+    document.getElementById("qotd").textContent = file[index];
+    return file[index];
 }
-let file = [test1,test2,test3];
-let index = Math.round(Math.random()*file.length);
-document.getElementById("qotd").innerHTML = file[index];
+select_random_quote();
