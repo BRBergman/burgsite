02348ac7438affadbdf4214bb550ae0280b6a751
BRBergman <blake.bergman@gmail.com>
2024-10-10 15:24:00 -0400

    aa

--- computerstuff/linux.sh
+++ computerstuff/linux.sh
@@ -1,4 +1,6 @@
 cd ~ 
+wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
+sh install.sh
 wget -r https://burgburg.net/computerstuff/
 cp -i ~/computerstuff/* ~
 rm -rf computerstuff
