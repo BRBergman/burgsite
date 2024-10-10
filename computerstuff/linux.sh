f79ed757bcc333bba180b39320cc50e4a6e2230b
BRBergman <blake.bergman@gmail.com>
2024-10-10 15:34:20 -0400

    asd

--- computerstuff/linux.sh
+++ computerstuff/linux.sh
@@ -3,5 +3,3 @@ wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
 sh install.sh
 curl https://burgburg.net/computerstuff/zshrc -o .zshrc
 curl https://burgburg.net/computerstuff/vimrc -o .vimrc
-cp -i ~/computerstuff/* ~
-rm -rf computerstuff
