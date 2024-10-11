8f251d21ed6b0901431f84231b694b011bdad218
BRBergman <blake.bergman@gmail.com>
2024-10-11 10:59:06 -0400

    bwaaa

--- computerstuff/run.sh
+++ computerstuff/run.sh
@@ -1,6 +1,6 @@
-cd ~ 
-wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
-sh install.sh
+#cd ~ 
+#wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
+#sh install.sh
 #Plug for vim
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
@@ -12,30 +12,30 @@ curl https://burgburg.net/computerstuff/zshrc -o .zshrc
 curl https://burgburg.net/computerstuff/vimrc -o .vimrc
 
 #cargo
-curl https://sh.rustup.rs -sSf | sh
+#curl https://sh.rustup.rs -sSf | sh
 
 # installs fnm (Fast Node Manager)
-curl -fsSL https://fnm.vercel.app/install | sh
+#curl -fsSL https://fnm.vercel.app/install | sh
 
 # activate fnm
-source ~/.zshrc
+#source ~/.zshrc
 
 # installs nvm (Node Version Manager)
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
+#curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
 
 # download and install Node.js (you may need to restart the terminal)
-nvm install 20
+#nvm install 20
 # verifies the right Node.js version is in the environment
-node -v # should print `v20.18.0`
+#node -v # should print `v20.18.0`
 
 # verifies the right npm version is in the environment
-npm -v # should print `10.8.2`
+#npm -v # should print `10.8.2`
 
 #kitty
-mkdir ~/.config/kitty
-curl  https://burgburg.net/kitty -o ~/.config/kitty.conf
+#mkdir ~/.config/kitty
+#curl  https://burgburg.net/kitty -o ~/.config/kitty.conf
 
 
 #fzf 
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
+#git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+#~/.fzf/install
