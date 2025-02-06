34a692569686b4b474ae85fc2ab25a03d96e5054
BRBergman <blake.bergman@gmail.com>
2025-02-05 21:44:05 -0500

    added quotw

--- index.js
+++ index.js
@@ -4,11 +4,22 @@ function SaulGoodmanBwawhAudio() {
     SaulGoodmanBwawh.play();
 }
 
-const file = ["bwaaa", "DO NOT THROW SOULS", "You're out of touch", "haiii", "Let me super break you", "Stellemarch is t4t", "amogus", "HATSUNE FUCKIN MIKUUUU", "It's Ikuyover", "It's Ryover", "It's murder, the solution is murder"];
+const quotes = ["bwaaa", 
+    "DO NOT THROW SOULS", 
+    "You're out of touch", 
+    "haiii", 
+    "Let me super break you", 
+    "Stellemarch is t4t", 
+    "amogus", 
+    "HATSUNE FUCKIN MIKUUUU", 
+    "It's Ikuyover", 
+    "It's Ryover", 
+    "It's murder, the solution is murder",
+    "When will you wear wigs?"];
 //todo: make read from seperate file that i can change without updating the website
 function select_random_quote() {
-    let index = Math.floor(Math.random() * file.length);
-    let quote = file[index];
+    let index = Math.floor(Math.random() * quotes.length);
+    let quote = quotes[index];
     document.getElementById("qotd").innerHTML = quote;
     return quote;
 }
